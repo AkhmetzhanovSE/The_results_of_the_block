01dace4d5b34bfddb19adfd6db712c60970207aa
AkhmetzhanovSE <133811821+AkhmetzhanovSE@users.noreply.github.com>
2023-05-18 21:46:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,3 +7,32 @@
 Console.WriteLine("Приветствую, сейчас будет выполнено решение с массивом уже заданным в алгоритм.");
 string[] array = { "Hello", "2", "world", ":-)", "222" };
 Console.Write("[ ");
+
+void Print(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+}
+Print(array);
+Console.Write("]");
+Console.WriteLine();
+Console.WriteLine("Далее массив будет отфильтрован согласно заданию: длина строк меньше, либо равна 3 символам.");
+filteredArray(array);
+void filteredArray(string[] array)
+{
+    Console.Write("[ ");
+    string[] newarray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarray[i] = array[i];
+            Console.Write(newarray[i] + " ");
+        }
+    }
+}
+Console.Write("]");
+Console.WriteLine();
+Console.WriteLine("Готово! Если необходимо отфильтровать другой массив, то внесите пожалуйста его вместо заданного в алгоритме массива.");
